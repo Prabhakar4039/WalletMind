ac92b8e96ab5dce7e9e66559dcab83644943e679
Prabhakar4039 <sprabhakar4039@gmail.com>
2026-04-07 00:51:22 +0530

    fix: remove unused Link import in Login.tsx to fix build error

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useAuthStore } from '../store/authStore';
 import { login, register } from '../services/authService';
 import GlassCard from '../components/GlassCard';
